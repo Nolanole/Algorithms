03aaf2ea1b470cddca4b0e583b97a3a10898fa0b
Nolanole <Jmancuso82@gmail.com>
2019-10-09 18:16:03 -0500

    thursday assignment

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,12 +1,21 @@
-#!/usr/bin/python
+ #!/usr/bin/python
 
 import sys
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+
+# pointless_zeroes=[]
+def eating_cookies(n, pointless_zeroes=[], cache = {0:1, 1:1, 2:2}):
+  if n in cache:
+    return cache[n]
+  else:
+    for i in range(max(cache)+1, n+1): 
+      cache[i] = eating_cookies(i-3) + eating_cookies(i-2) + eating_cookies(i-1)
+    return cache[n]
+
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,44 @@
 
 import sys
 
+# def rock_paper_scissors(n):
+#     rps = ['rock', 'paper', 'scisssors']
+
+#     if n == 1:
+#         return [['rock'], ['paper'], ['scissors']]
+#     else:
+#         base = rock_paper_scissors(n-1)
+#         new_list = []
+#         for i in range(3):
+#             for j in range(len(base)):
+#                 new_list += [base[j][:]]
+#         for i in range(len(new_list)):
+#             if i < len(new_list)/3:
+#                 new_list[i].append('rock')
+#             elif i < len(new_list)*2/3:
+#                 new_list[i].append('paper')
+#             else:
+#                 new_list[i].append('scissors')
+#         return new_list
+
 def rock_paper_scissors(n):
-  pass 
+    rps = [['rock'], ['paper'], ['scissors']]
+    if n == 1:
+        return rps
+    else:
+        base = rock_paper_scissors(n-1)
+        new_list = []
+        for i in range(3):
+            for j in range(len(base)):
+                new_list += [base[j][:]]
+        for i in range(len(new_list)):
+            if i < len(new_list)/3:
+                new_list[i].insert(0, 'rock')
+            elif i < len(new_list)*2/3:
+                new_list[i].insert(0, 'paper')
+            else:
+                new_list[i].insert(0, 'scissors')
+        return new_list
 
 
 if __name__ == "__main__":
